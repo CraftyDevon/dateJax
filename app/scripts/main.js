fd65eee9eb841c8a9e30eb33727538b28ad001c0
Devon Hicks <devon@devonhicks.com>
2014-05-14 09:17:51 -0400

    Version 1.2 using the crummy YellowBook Api

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -16,6 +16,7 @@ var dateChoose = {
   initEvents: function() {
 
     this.getRest();
+    this.getAct();
 
 
   },
@@ -35,9 +36,9 @@ var dateChoose = {
       e.preventDefault();
       console.log("im working");
 
-      // var myZip = $(".rzip").val();
+       var myZip = $(".rzip").val();
 
-      // console.log(myZip);
+       console.log(myZip);
  
 
   
@@ -45,15 +46,23 @@ var dateChoose = {
       
 
       $.ajax({
-      url: "https://maps.googleapis.com/maps/api/place/details/json?reference=CmRYAAAAciqGsTRX1mXRvuXSH2ErwW-jCINE1aLiwP64MCWDN5vkXvXoQGPKldMfmdGyqWSpm7BEYCgDm-iv7Kc2PF7QA7brMAwBbAcqMr5i1f4PwTpaovIZjysCEZTry8Ez30wpEhCNCXpynextCld2EBsDkRKsGhSLayuRyFsex6JA6NPh9dyupoTH3g&sensor=true&key=AIzaSyCNJ06Z4hQ7_ORxRrtV7nTapM3FcddmrYE",
+     // url: "https://maps.googleapis.com/maps/api/place/details/json?reference=CmRYAAAAciqGsTRX1mXRvuXSH2ErwW-jCINE1aLiwP64MCWDN5vkXvXoQGPKldMfmdGyqWSpm7BEYCgDm-iv7Kc2PF7QA7brMAwBbAcqMr5i1f4PwTpaovIZjysCEZTry8Ez30wpEhCNCXpynextCld2EBsDkRKsGhSLayuRyFsex6JA6NPh9dyupoTH3g&sensor=false&key=AIzaSyCNJ06Z4hQ7_ORxRrtV7nTapM3FcddmrYE",
+      url: "http://pubapi.atti.com/search-api/search/devapi/search?searchloc=" + myZip + "&term=food&format=json&sort=distance&radius=50&listingcount=10&key=lxs36m569x",
       type: "GET",
       dataType: "jsonp",
       error: function (jqXHR, status, error){
         alert ("GET Failed");
       },
       success: function(data){
+
+
+        var randomRest = Math.floor((Math.random()*data.searchResult.searchListings.searchListing.length)+1);
+
+        $(".restGen").html("<h4>" +data.searchResult.searchListings.searchListing[randomRest].businessName + " shall be your establishment of choice." + "</h4>");
+        $(".restGen").append('<a href="' + data.searchResult.searchListings.searchListing[randomRest].businessNameURL + ' ">' +  'Visit their website' + '</a>');
+
      
-        alert ("YOU WIN THE INTERNET");
+    
 
 
       } //end success
@@ -66,19 +75,44 @@ var dateChoose = {
 
   getAct: function(){
 
+       $(".actzip").click(function(e) {
+      e.preventDefault();
+      console.log("im working");
+
+       var myZip2 = $(".azip").val();
+
+       console.log(myZip2);
+ 
+
+  
+
+      
+
       $.ajax({
-      url: "http://tiy-fee-rest.herokuapp.com/collections/devon/" + todayid,
+     // url: "https://maps.googleapis.com/maps/api/place/details/json?reference=CmRYAAAAciqGsTRX1mXRvuXSH2ErwW-jCINE1aLiwP64MCWDN5vkXvXoQGPKldMfmdGyqWSpm7BEYCgDm-iv7Kc2PF7QA7brMAwBbAcqMr5i1f4PwTpaovIZjysCEZTry8Ez30wpEhCNCXpynextCld2EBsDkRKsGhSLayuRyFsex6JA6NPh9dyupoTH3g&sensor=false&key=AIzaSyCNJ06Z4hQ7_ORxRrtV7nTapM3FcddmrYE",
+      url: "http://pubapi.atti.com/search-api/search/devapi/search?searchloc=" + myZip2 + "&term=movies&format=json&sort=distance&radius=150&listingcount=10&key=lxs36m569x",
       type: "GET",
-      error: function (data){
-        //alert ("DELETE Failed");
+      dataType: "jsonp",
+      error: function (jqXHR, status, error){
+        alert ("GET Failed");
       },
       success: function(data){
-        //alert ("Delete good!");
 
-      }
-    });
 
-    }
+        var randomAct = Math.floor((Math.random()*data.searchResult.searchListings.searchListing.length)+1);
+
+        $(".restGen").html(data.searchResult.searchListings.searchListing[randomAct].businessName);
+
+     
+    
+
+
+      } //end success
+      });
+
+      })
+
+    }//end of getAct function
   
 
   
